9d04d624509c4711bf9ccf303d49a808065f52c6
Bruno Henrique Ribeiro <bhribeiro99@gmail.com>
2026-08-31 17:17:02 -0300

    Atualizacao criando pastas e js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const classes = {
+    programacao: {
+        nome: "Programação",
+        icone: "💻",
+        descricao: "Criar sistemas, sites e jogos."
+    },
+
+    design: {
+        nome: "Design",
+        icone: "🎨",
+        descricao: "Criar a aparência e a experiência que o usuário tem ao interagir com um sistema."
+    },
+
+    redes: {
+        nome: "Redes e Internet",
+        icone: "🌐",
+        descricao: "Conectar computadores, celulares e sistemas."
+    },
+
+    seguranca: {
+        nome: "Cibersegurança",
+        icone: "🔐",
+        descricao: "Proteger sistemas e informações."
+    },
+
+    suporte: {
+        nome: "Tecnologia e Suporte",
+        icone: "🛠️",
+        descricao: "Configurar, resolver problemas e ajudar pessoas com tecnologia."
+    }
+};
